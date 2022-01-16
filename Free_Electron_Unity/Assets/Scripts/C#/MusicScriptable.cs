8daa78f00345b810e5e559fd43d801614c2a226b
Ahmadji <a.ursulet@student.isartdigital.com>
2022-01-16 18:38:03 +0100

    Bug Fix + Refacto/Cleaning
    Bug controls + bug free alectron

--- Free_Electron_Unity/Assets/Scripts/C#/MusicScriptable.cs
+++ Free_Electron_Unity/Assets/Scripts/C#/MusicScriptable.cs
@@ -5,18 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Music", menuName = "FreeElectron/Music")]
 public class MusicScriptable : ScriptableObject
 {
-    /*
-    [Header("Musics")]
-    public AudioClip mainMenuMusic;
-    public AudioClip tutorialMusic;
-    public AudioClip level1Music;
-    public AudioClip level2Music;
-    public AudioClip level3Music;
-    public AudioClip level4Music;
-    public AudioClip level5Music;
-    public AudioClip gameOverMusic;
-    public AudioClip gameWonMusic;
-    */
-
     public Sound[] musics;
 }
--- Free_Electron_Unity/Assets/Scripts/C#/SFXScriptable.cs
+++ Free_Electron_Unity/Assets/Scripts/C#/SFXScriptable.cs
@@ -6,88 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SFX", menuName = "FreeElectron/SFX")]
 public class SFXScriptable : ScriptableObject
 {
-    /*
-    [Header("In the Continuum")]
-    public AudioClip continuumDoubleTap;
-    public AudioClip continuumRotating;
-    public AudioClip continuumMoving;
-
-    [Header("Linked To Atom")]
-    public AudioClip linkedDoubleTap;
-    public AudioClip linkedOneTap;
-    public AudioClip linkedRotating;
-    public AudioClip linkedMoving;
-    public AudioClip reloadingEnergy;
-    public AudioClip collidingAlectron;
-    public AudioClip expulsion;
-
-    [Header("Checkpoints")]
-    public AudioClip takingCP;
-
-    [Header("Menu")]
-    public AudioClip playButtonClick;
-    public AudioClip exitButtonClick;
-    public AudioClip buttonSelection;
-    public AudioClip buttonClick;
-    public AudioClip pauseMenuActivate;
-    public AudioClip pauseMenuDeactivate;
-
-
-    public AudioClip[] clips;
-    */
-
-    /*
-    [Header("In the Continuum")]
-    public Sound continuumDoubleTap;
-    public Sound continuumRotating;
-    public Sound continuumMoving;
-
-    [Header("Linked To Atom")]
-    public Sound linkedDoubleTap;
-    public Sound linkedOneTap;
-    public Sound linkedRotating;
-    public Sound linkedMoving;
-    public Sound reloadingEnergy;
-    public Sound collidingAlectron;
-    public Sound expulsion;
-
-    [Header("Checkpoints")]
-    public Sound takingCP;
-
-    [Header("Menu")]
-    public Sound playButtonClick;
-    public Sound exitButtonClick;
-    public Sound buttonSelection;
-    public Sound buttonClick;
-    public Sound pauseMenuActivate;
-    public Sound pauseMenuDeactivate;
-    */
     [Header("Localized Sounds")]
     [Header("Checkpoints")]
     public AudioClip takingCP;
 
     
     public Sound[] sounds;
-
-    /*
-    private void OnValidate()
-    {
-        
-        clips = new AudioClip[] {
-        continuumDoubleTap, continuumRotating, continuumMoving,
-        linkedDoubleTap, linkedOneTap, linkedRotating, linkedMoving, reloadingEnergy, collidingAlectron, expulsion,
-        takingCP,
-        playButtonClick, exitButtonClick, buttonSelection, buttonClick, pauseMenuActivate, pauseMenuDeactivate};
-        
-
-        
-        sounds = new Sound[] {
-        continuumDoubleTap, continuumRotating, continuumMoving,
-        linkedDoubleTap, linkedOneTap, linkedRotating, linkedMoving, reloadingEnergy, collidingAlectron, expulsion,
-        takingCP,
-        playButtonClick, exitButtonClick, buttonSelection, buttonClick, pauseMenuActivate, pauseMenuDeactivate};
-        
-    }
-    */
-
 }
