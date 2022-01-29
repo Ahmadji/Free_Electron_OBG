2065014de3c5fba3324bcc29910f279123cb3a9c
Ahmadji <a.ursulet@student.isartdigital.com>
2022-01-29 15:49:11 +0100

    Uncompleted dialogue system

--- Free_Electron_Unity/Assets/Scripts/C#/Dialogue/Dialogue.cs
+++ Free_Electron_Unity/Assets/Scripts/C#/Dialogue/Dialogue.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 [System.Serializable] // So that the class appears in the inspector
 public class Dialogue
 {
     public string name;
+    public TextMeshPro textMesh;
 
     [TextArea(3, 10)]
     public string[] sentences;
--- Free_Electron_Unity/Assets/Scripts/C#/Dialogue/DialogueManager.cs
+++ Free_Electron_Unity/Assets/Scripts/C#/Dialogue/DialogueManager.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     // Queue is a FIFO collection (first in first out)
     // Fits well for what we want to do
-
     private Queue<string> sentences;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // Initialize
         sentences = new Queue<string>();
@@ -20,9 +21,31 @@ public class DialogueManager : MonoBehaviour
     {
         Debug.Log("Starting conversation with " + dialogue.name);
 
-        foreach(string sentence in dialogue.sentences)
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
         {
-            Debug.Log(sentence);
+            sentences.Enqueue(sentence);
         }
+
+        DisplayNextSentence(dialogue.textMesh);
+    }
+
+    public void DisplayNextSentence(TextMeshPro textMesh)
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        textMesh.text = sentence;
+
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
     }
 }
